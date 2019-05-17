32e5dca024109b31af0b475c11e3fe9eaf1bdf80
杨浩然 <bill-billion@outlook.com>
2019-05-17 10:05:47 +0800

    登录、注册页面实现

--- src/scripts/login/login.js
+++ src/scripts/login/login.js
@@ -1,1 +1,67 @@
-console.log("this is login");
+var strength = 0;
+let{
+    usr,pwd,login,form
+}={
+    usr : document.getElementById("usrname"),
+    pwd : document.getElementById("usrpwd"),
+    login : document.getElementById("btn"),
+    form : document.getElementById("login_form")
+}
+usr.addEventListener("blur",judgeUserName);
+pwd.addEventListener("blur",judgeUserPwd);
+function changeNormal(){
+    usr.parentElement.className = "input-box";
+    usr.nextElementSibling.className = "normal"
+    strength ++;
+}
+function judgeUserName(){
+    let usrValue = usr.value,
+    //Node.nextSibling 是一个只读属性，返回其父节点的 childNodes 列表中紧跟在其后面的节点，如果指定的节点为最后一个节点，则返回 null。
+        next = usr.nextElementSibling,
+        parents = usr.parentElement;
+    if(usrValue.length <= 4 || usrValue.length >=20){
+        next.className = "warn_l";
+        parents.className = "input-box warn";
+        next.innerHTML = "用户名不能为空";
+        return false;
+    }
+    changeNormal(); 
+    console.log(strength );
+}
+function judgeUserPwd(){
+    let pwdValue = pwd.value,
+        next = pwd.nextElementSibling,
+        parents = pwd.parentElement;
+    if(pwdValue.length <= 6){
+        parents.className = "input-box warn";
+        next.className = "warn_l";
+        next.innerHTML = "密码不能为空";
+        return false;
+    }
+    changeNormal();
+    console.log(strength);
+}
+form.onsubmit = function(evt){
+    var e = evt || window.event;
+    e.preventDefault();
+}
+// 获取数据发送请求;
+login.onclick = function(evt){
+    var usrname = usr.value;
+    var usrpwd = pwd.value;
+    console.log(usrname,usrpwd)
+    xhrPost("./server/login.php",{usrname,usrpwd})
+    .then((res)=>{
+        var obj = JSON.parse(res);
+        console.log(obj);
+        console.log(obj.stateCode);
+        if(obj.stateCode == 1){
+            jump();
+        }else{
+            alert("失败");
+        }
+    })
+}
+function jump(){
+    window.location.href="./index.html"
+}
--- src/scripts/register/register.js
+++ src/scripts/register/register.js
@@ -1,1 +1,91 @@
-console.log("this is register")
+var strength = 0;
+let{usr,pwd,rPwd,reg,form,tag}={
+    usr : document.getElementById("usr"),
+    pwd : document.getElementById("pwd"),
+    rPwd : document.getElementById("r_pwd"),
+    reg : document.getElementById("btn"),
+    form : document.getElementById("register-form"),
+    tag : document.querySelector("#tag")
+}
+usr.addEventListener("blur",judgeUserName);
+pwd.addEventListener("blur",judgeUserPwd);
+rPwd.addEventListener("blur",reJudgeUserPwd);
+reg.addEventListener("click",submitMsg);
+function changeNormal(){
+    usr.parentElement.className = "input-box";
+    usr.nextElementSibling.className = "normal"
+    strength ++;
+}
+function judgeUserName(){
+    let usrValue = usr.value,
+    //Node.nextSibling 是一个只读属性，返回其父节点的 childNodes 列表中紧跟在其后面的节点，如果指定的节点为最后一个节点，则返回 null。
+        next = usr.nextElementSibling,
+        parents = usr.parentElement,
+        usrReg = /^[a-z\u2e80-\u9fff\-_0-9]{4,20}$/i;
+    if(usrValue.length <= 4 || usrValue.length >=20){
+        next.className = "warn_l";
+        parents.className = "input-box warn";
+        next.innerHTML = "请输入4-20位的用户名";
+        return false;
+    }else if(!usrReg.test(usrValue)){
+        next.innerHTML = "仅支持中文，英文，数字，字母，-，_ 的4~20位字符";
+        next.className = " error_l";
+        parents.className = "input-box error";
+        return false;
+    }else if(/^\d+$/.test(usrValue)){
+        next.innerHTML = "用户名不能为纯数字";
+        next.className = " warn_l";
+        parents.className = "input-box warn";
+        return false;
+    }
+    changeNormal(); 
+    console.log(strength );
+}
+function judgeUserPwd(){
+    let pwdValue = pwd.value,
+        next = pwd.nextElementSibling,
+        parents = pwd.parentElement;
+    if(pwdValue.length <= 6){
+        parents.className = "input-box warn";
+        next.className = "warn_l";
+        next.innerHTML = "密码需要大于6位";
+        return false;
+    }else{
+        changeNormal();
+    }
+    console.log(strength);
+}
+function reJudgeUserPwd(){
+    console.log(rPwd.value)
+    let rPwdValue = rPwd.value,
+    //Node.nextSibling 是一个只读属性，返回其父节点的 childNodes 列表中紧跟在其后面的节点，如果指定的节点为最后一个节点，则返回 null。
+        next = rPwd.nextElementSibling;
+        parents = rPwd.parentElement;
+    if(rPwdValue != pwd.value){
+        parents.className = "input-box warn";
+        next.className = "warn_l";
+        next.innerHTML = "两次密码不一致";
+        return false;
+    }
+}
+function submitMsg(){
+    let usrname = usr.value,
+        usrpwd = pwd.value,
+        usrlike = tag.value;
+    console.log(usrname,usrpwd,usrlike);
+    xhrPost("./server/register.php",{usrname,usrpwd,usrlike})
+    .then((res)=>{
+        var obj = JSON.parse(res);
+        console.log(obj);
+        console.log(obj.stateCode);
+        if(obj.stateCode == 1){
+            alert("成功，即将跳转登陆页面");
+            jump();
+        }else{
+            alert("失败");
+        }
+    })
+}
+function jump(){
+    window.location.href="./login.html"
+}
