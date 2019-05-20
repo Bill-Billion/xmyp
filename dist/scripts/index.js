368f1c7751959a7e8fb9f9e4a0ada1c607c56e49
杨浩然 <bill-billion@outlook.com>
2019-05-20 20:14:54 +0800

    秒杀页添加

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,5 +1,55 @@
 "use strict";
 
-console.log(1);
-console.log("this is index-nav.js");
-console.log("this is index.js");
+var totalTime = 3600;
+var count = setInterval(function () {
+  totalTime--;
+
+  if (totalTime == 0) {
+    clearInterval(count);
+    return true;
+  }
+
+  var c_hours = "<span>" + parseInt(totalTime / 3600) + "</span>";
+
+  if (parseInt(totalTime / 3600) < 10) {
+    c_hours = "<span> 0" + parseInt(totalTime / 3600) + "</span>";
+  }
+
+  hour.innerHTML = c_hours;
+  var c_minutes = "<span>" + parseInt(totalTime / 60 % 60) + "</span>";
+
+  if (parseInt(totalTime / 60 % 60) < 10) {
+    c_minutes = "<span> 0" + parseInt(totalTime / 60 % 60) + "</span>";
+  }
+
+  minute.innerHTML = c_minutes;
+  var c_seconds = "<span>" + parseInt(totalTime % 60) + "</span>";
+
+  if (parseInt(totalTime % 60) < 10) {
+    c_seconds = "<span> 0" + parseInt(totalTime % 60) + "</span>";
+  }
+
+  second.innerHTML = c_seconds;
+}, 1000); // let count = setInterval(countDown(),1000);
+// function countDown(count){
+//     totalTime--;
+//     if(totalTime == 0){
+//         clearInterval(count);
+//         return true;
+//     }
+//     var c_hours = "<span>" + parseInt(totalTime/3600) +"</span>"
+//         if(parseInt(totalTime/3600) <10){
+//             c_hours = "<span> 0" + parseInt(totalTime/3600) +"</span>";
+//         }
+//     hour.innerHTML = c_hours;
+//     var c_minutes = "<span>" + parseInt((totalTime/60)%60) +"</span>";
+//         if(parseInt((totalTime/60)%60) < 10){
+//             c_minutes =  "<span> 0" + parseInt((totalTime/60)%60) +"</span>";
+//         }
+//     minute.innerHTML = c_minutes;
+//     var c_seconds = "<span>" + parseInt(totalTime%60) +"</span>";
+//         if(parseInt(totalTime%60) <10){
+//             c_seconds = "<span> 0" + parseInt(totalTime%60) +"</span>";
+//         }
+//     second.innerHTML = c_seconds;
+// }
